c7895f0f4ab9efb277bab6959ca34fc85951a2d1
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2023-12-02 18:00:34 +0900

    refactor/ONSEN-10_不要な記述を削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import './globals.css'
 import { notojp } from "./utiles/fonts";
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Sidebar from './components/Sidebar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
