aeec2af93d89b84447e14d33b7c360e2adda288c
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2023-12-30 13:47:41 +0900

    feat/ONSEN-27_都道府県に関するコンポーネントを作成

---
+++ app/components/selects/PrefectureSelect.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Select, SelectItem } from '@nextui-org/react'
+
+interface Attributes {
+  id: number
+  str_key: string
+  name: string
+}
+
+interface Prefecture {
+  attributes: Attributes
+}
+
+interface PrefectureSelectProps {
+  label: string
+  placeholder: string;
+  prefectures: Prefecture[]
+  variant: 'flat' | 'bordered' | 'faded' | 'underlined' | undefined
+  errorMessage?: string
+  isInvalid?: boolean
+  isRequired?: boolean
+  className?: string
+}
+
+const PrefectureSelect: React.FC<PrefectureSelectProps> = ({ label, placeholder, prefectures, variant, className, isRequired, errorMessage, isInvalid, ...field }) => {
+  return (
+    <Select
+      label={label}
+      placeholder={placeholder}
+      selectionMode="single"
+      variant={variant}
+      className={className}
+      isRequired={isRequired}
+      errorMessage={errorMessage}
+      isInvalid={isInvalid}
+      {...field}
+    >
+      {prefectures.map(prefecture => (
+        <SelectItem key={prefecture.attributes.id} value={prefecture.attributes.str_key} textValue={prefecture.attributes.name}>
+          {prefecture.attributes.name}
+        </SelectItem>
+      ))}
+    </Select>
+  )
+}
+
+export default PrefectureSelect
